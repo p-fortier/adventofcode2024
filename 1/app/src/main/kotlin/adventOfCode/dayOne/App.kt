f8b0dac729eef6ea732c45f1ea9b74ab8d04215e
Patrik Fortier <fortier.patrik@gmail.com>
2024-12-01 22:19:53 +0100

    dayOne completed

--- 1/app/src/main/kotlin/adventOfCode/dayOne/App.kt
+++ 1/app/src/main/kotlin/adventOfCode/dayOne/App.kt
@@ -5,20 +5,33 @@ import kotlin.math.absoluteValue
 class App()
 
 fun main() {
-    //recover lists from txt file
-    val input = object {}.javaClass.getResource("/input.txt").readText()
-    val results = """\d+""".toRegex().findAll(input)
-    val listOne = results.toList().filterIndexed { index, word ->
-        index % 2 == 0
-    }.map { it.value.toInt() }
-    val listTwo = results.toList().filterIndexed { index, word ->
-        index % 2 == 1
-    }.map { it.value.toInt() }
 
-    // sort lists, compare and sum distances
-    val distance = listOne.sorted().zip(listTwo.sorted()).map {
-        (it.first - it.second).absoluteValue
-    }.sum()
-    println("Distance: $distance")
+    // intro fetch data
+    try {
+        val input = object {}.javaClass.getResource("/input.txt")!!.readText()
 
+        val results = """\d+""".toRegex().findAll(input)
+        val listOne = results
+            .toList()
+            .filterIndexed { index, word -> index % 2 == 0 }
+            .map { it.value.toInt() }
+        val listTwo = results
+            .toList()
+            .filterIndexed { index, word -> index % 2 == 1 }
+            .map { it.value.toInt() }
+
+        //part 1 distance score
+        val distance = listOne.sorted().zip(listTwo.sorted()).map {
+            (it.first - it.second).absoluteValue
+        }.sum()
+        println("Distance: $distance")
+
+        //part 2 similarity score
+        val similarity = listOne.map { one ->
+            one * listTwo.count { it.equals(one) }
+        }.sum()
+        println("Similarity: $similarity")
+    } catch (_: NullPointerException) {
+        println("Wrong file")
+    }
 }
