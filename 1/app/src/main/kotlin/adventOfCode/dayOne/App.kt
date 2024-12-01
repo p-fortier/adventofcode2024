c652b0ba32e3f671906a874606d7fc75001705fd
Patrik Fortier <fortier.patrik@gmail.com>
2024-12-01 22:35:11 +0100

    solution 2 less Loc but not better

--- 1/app/src/main/kotlin/adventOfCode/dayOne/App.kt
+++ 1/app/src/main/kotlin/adventOfCode/dayOne/App.kt
@@ -10,26 +10,35 @@ fun main() {
     try {
         val input = object {}.javaClass.getResource("/input.txt")!!.readText()
 
-        val results = """\d+""".toRegex().findAll(input)
-        val listOne = results
-            .toList()
-            .filterIndexed { index, word -> index % 2 == 0 }
+        val results = """\d+""".toRegex()
+            .findAll(input)
             .map { it.value.toInt() }
-        val listTwo = results
-            .toList()
-            .filterIndexed { index, word -> index % 2 == 1 }
-            .map { it.value.toInt() }
+            .withIndex()
+            .groupBy { it.index % 2 == 0 }
+
+        val listOne = results[true]
+        val listTwo = results[false]
+
+//        val listOne = results
+//            .toList()
+//            .filterIndexed { index, word -> index % 2 == 0 }
+//            .map { it.value.toInt() }
+//        val listTwo = results
+//            .toList()
+//            .filterIndexed { index, word -> index % 2 == 1 }
+//            .map { it.value.toInt() }
 
         //part 1 distance score
-        val distance = listOne.sorted().zip(listTwo.sorted()).map {
-            (it.first - it.second).absoluteValue
+        val distance = results[true]!!.sortedBy { it.value }
+            .zip(results[false]!!.sortedBy {it.value})
+            .map { (it.first.value - it.second.value).absoluteValue
         }.sum()
         println("Distance: $distance")
 
         //part 2 similarity score
-        val similarity = listOne.map { one ->
-            one * listTwo.count { it.equals(one) }
-        }.sum()
+        val similarity = listOne?.map { one ->
+            one.value * (listTwo?.count { it.value.equals(one.value) }!!)
+        }?.sum()
         println("Similarity: $similarity")
     } catch (_: NullPointerException) {
         println("Wrong file")
